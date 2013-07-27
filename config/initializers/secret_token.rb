d7909669da92f58ad0c945d14499c39a9084f8c4
chris crowd <zent29@gmail.com>
2013-07-26 23:10:19 -0400

    main view working with bootstrap header

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectTracker::Application.config.secret_token = '5fdfc4aa6d3f1a6980dba3d3a6ca19277e8bb33597a82fcaeca1d1275c3775b92c189c9cd80be47a6da2fdaab3d744533d56600d50aa63c5ee0c266d69e75bd5'
